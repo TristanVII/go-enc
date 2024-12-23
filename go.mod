12e6a27b5720272d9320dfd46cab11563ab98e56
Tristan Davis <Tristandavis888@gmail.com>
2024-12-23 10:52:20 -0800

    added encrypt, decrypt logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-enc/main
 
 go 1.21.4
+
+require golang.org/x/crypto v0.31.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"errors"
 	"fmt"
 	"io"
+
+	"golang.org/x/crypto/nacl/secretbox"
 )
 
 const (
 	KeySize   = 32
 	NonceSize = 24
+)
+
+var (
+	ErrEncrypt = errors.New("secret: encryption failed")
+	ErrDecrypt = errors.New("secret: decryption failed")
 )
 
 // GenerateKey creates a new random secret key
@@ -36,7 +45,56 @@ func generateNonce() (*[NonceSize]byte, error) {
 	fmt.Printf("nonce: %v\n", nonce)
 	return nonce, nil
 }
+
+func encrypt(key *[KeySize]byte, message []byte) ([]byte, error) {
+	nonce, err := generateNonce()
+	if err != nil {
+		return nil, ErrEncrypt
+	}
+
+	out := make([]byte, len(nonce))
+	copy(out, nonce[:])
+	out = secretbox.Seal(out, message, nonce, key)
+	return out, nil
+	
+}
+
+// decrypt expects the encrypted message with the nonce
+func decrypt(key *[KeySize]byte, message []byte) ([]byte, error) {
+	if len(message) < (NonceSize + secretbox.Overhead) {
+		return nil, ErrDecrypt
+	}
+	var nonce [NonceSize]byte
+	copy(nonce[:], message[:NonceSize])
+	out, ok := secretbox.Open(nil, message[NonceSize:], &nonce, key)
+	if !ok {
+		return nil, ErrDecrypt
+	}
+	return out, nil
+}
+
+
 func main() {
-	generateKey()
-	generateNonce()
+	message := []byte("Test123")
+	key, err := generateKey()
+	if err != nil {
+		fmt.Println("Failed to generate key")
+		return
+	}
+	encryptedMessage, err := encrypt(key, message)
+	if err != nil {
+		fmt.Println("Failed to encrypt")
+		return
+	}
+	fmt.Printf("encryptedMessage: %s\n", string(encryptedMessage))
+	decryptedMessage, err := decrypt(key, encryptedMessage)
+	if err != nil {
+		fmt.Println("Failed to decrypt")
+		return
+	}
+	fmt.Printf("decryptedMessage: %s\n", string(decryptedMessage))
+	if !bytes.Equal(message, decryptedMessage) {
+		fmt.Println("Encrypted message and decrypted message do not match")
+		return
+	}
 }
